7a552872f569b9024a29c347d54c4ae6761c6629
abrahamkivosh <abrahamkivosh@gmail.com>
2022-04-26 09:27:39 +0300

    added navbar to vue

---
+++ resources/js/components/clientApp/Index.vue
@@ -0,0 +1,20 @@
+<template>
+<span>
+    <nav-bar />
+
+</span>
+
+
+</template>
+
+<script>
+import NavBar from './NavBar.vue'
+export default {
+  components: { NavBar },
+
+}
+</script>
+
+<style>
+
+</style>
--- resources/views/layouts/client/app.blade.php
+++ resources/views/layouts/client/app.blade.php
@@ -19,7 +19,7 @@
     <!-- font awesome CSS -->
     <link rel="stylesheet" href="{{ asset('client/css/all.css') }}">
     <!-- nice select CSS -->
-  <link rel="stylesheet" href="{{ asset('client/css/nice-select.css')}}">
+    <link rel="stylesheet" href="{{ asset('client/css/nice-select.css') }}">
     <!-- flaticon CSS -->
     <link rel="stylesheet" href="{{ asset('client/css/flaticon.css') }}">
     <link rel="stylesheet" href="{{ asset('client/css/themify-icons.css') }}">
@@ -29,118 +29,31 @@
     <link rel="stylesheet" href="{{ asset('client/css/slick.css') }}">
     <!-- style CSS -->
     <link rel="stylesheet" href="{{ asset('client/css/style.css') }}">
-      <!-- swiper CSS -->
-  <link rel="stylesheet" href="{{ asset('client/css/slick.css') }}">
-  <link rel="stylesheet" href="{{ asset('client/css/price_rangs.css') }}">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="{{ asset('client/css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/css/price_rangs.css') }}">
 
-      <!-- Scripts -->
-      <script src="{{ asset('js/app.js') }}" defer></script>
 
-      <!-- Fonts -->
-      <link rel="dns-prefetch" href="//fonts.gstatic.com">
-      <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-      <!-- Styles -->
-      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
 
-<body>
-    <!--::header part start::-->
-    <header class="main_menu home_menu">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-12">
-                    <nav class="navbar navbar-expand-lg navbar-light">
-                        <a class="navbar-brand" href="index.html"> <img src="client/img/logo.png" alt="logo"> </a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="menu_icon"><i class="fas fa-bars"></i></span>
-                        </button>
+<body  >
+    <main id="app">
+        <client-app ></client-app>
+    </main>
 
-                        <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('client.index') }}">Home</a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_1"
-                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Shop
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
-                                        <a class="dropdown-item" href="">category 1</a>
-                                        <a class="dropdown-item" href="">category 2</a>
-                                        <a class="dropdown-item" href="">category 3</a>
-                                    </div>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_3"
-                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Account
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
-                                        <a class="dropdown-item" href="login.html"> login</a>
-                                        <a class="dropdown-item" href="login.html"> Register</a>
-                                        <a class="dropdown-item" href="tracking.html">tracking</a>
-                                        <a class="dropdown-item" href="confirmation.html">confirmation</a>
-                                        <a class="dropdown-item" href="elements.html">Orders</a>
-                                    </div>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_2"
-                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Help
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
-                                        <a class="dropdown-item" href="blog.html"> Help Center</a>
-                                        <a class="dropdown-item" href="blog.html"> Order cancellation</a>
-                                        <a class="dropdown-item" href="blog.html"> Place & Track orders</a>
-                                    </div>
-                                </li>
 
-                                <li class="nav-item">
-                                    <a class="nav-link" href="contact.html">Contact</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="hearer_icon d-flex">
-                            <a id="search_1" href="javascript:void(0)"><i class="ti-search"></i></a>
-                            <a href=""><i class="ti-heart"></i></a>
-                            <div class="dropdown cart">
-                                <a class="dropdown-toggle" href="#" id="navbarDropdown3" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="fas fa-cart-plus"></i>
-                                </a>
-                                <!-- <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <div class="single_product">
-
-                                    </div>
-                                </div> -->
-
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </div>
-        <div class="search_input" id="search_input_box">
-            <div class="container ">
-                <form class="d-flex justify-content-between search-inner">
-                    <input type="text" class="form-control" id="search_input" placeholder="Search Here">
-                    <button type="submit" class="btn"></button>
-                    <span class="ti-close" id="close_search" title="Close Search"></span>
-                </form>
-            </div>
-        </div>
-    </header>
-    <!-- Header part end-->
-
-        @yield('content')
+    {{-- @yield('content') --}}
 
     <!-- subscribe_area part start-->
-    <section class="subscribe_area section_padding">
+    {{-- <section class="subscribe_area section_padding">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-7">
@@ -159,7 +72,7 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> --}}
     <!--::subscribe_area part end::-->
 
     <!-- subscribe_area part start-->
@@ -167,7 +80,7 @@
     <!--::subscribe_area part end::-->
 
     <!--::footer_part start::-->
-    <footer class="footer_part">
+    {{-- <footer class="footer_part">
         <div class="container">
             <div class="row justify-content-around">
                 <div class="col-sm-6 col-lg-2">
@@ -241,9 +154,16 @@
                 <div class="row">
                     <div class="col-lg-8">
                         <div class="copyright_text">
-                            <P><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></P>
+                            <P>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                Copyright &copy;
+                                <script>
+                                    document.write(new Date().getFullYear());
+                                </script> All rights reserved | This template is made with <i
+                                    class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com"
+                                    target="_blank">Colorlib</a>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            </P>
                         </div>
                     </div>
                     <div class="col-lg-4">
@@ -259,7 +179,7 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                 </div>
             </div>
         </div>
-    </footer>
+    </footer> --}}
     <!--::footer_part end::-->
 
     <!-- jquery plugins here-->
@@ -290,6 +210,8 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
     <script src="{{ asset('client/js/price_rangs.js') }}"></script>
     <!-- custom js -->
     <script src="{{ asset('client/js/custom.js') }}"></script>
+       <!-- Scripts -->
+       <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 
 </html>
