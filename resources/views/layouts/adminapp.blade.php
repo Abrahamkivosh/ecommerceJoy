74865e46fa39e17b1df5246102483cc02c03de87
abrahamkivosh <abrahamkivosh@gmail.com>
2022-11-23 01:14:59 +0300

    change

--- resources/views/layouts/adminapp.blade.php
+++ resources/views/layouts/adminapp.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Odds & Ends</title>
+    <title>JK-JIJI</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
